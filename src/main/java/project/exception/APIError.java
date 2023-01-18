234980d66e500553e6fde9445518cdc3b0a1a6de
gyunny <needle12s@gmail.com>
2023-01-18 17:27:36 +0900

    feat : Response & lombok setting

---
+++ src/main/java/project/exception/APIError.java
@@ -0,0 +1,16 @@
+package project.exception;
+
+public class APIError extends Error{
+
+    private final String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public APIError(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+}
