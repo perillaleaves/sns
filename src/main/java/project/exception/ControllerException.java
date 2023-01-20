08a5a242ee58e88993138370346287c482daf371
gyunny <needle12s@gmail.com>
2023-01-20 15:16:26 +0900

    feat : Exception Handliing

--- src/main/java/project/exception/ControllerException.java
+++ src/main/java/project/exception/ControllerException.java
@@ -3,13 +3,14 @@ package project.exception;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import project.response.ErrorResponse;
+import project.response.Response;
 
 @RestControllerAdvice
 public class ControllerException {
 
     @ExceptionHandler(APIError.class)
-    public ErrorResponse handleAPIErrorException(APIError e) {
-        return new ErrorResponse(e.getCode(), e.getMessage());
+    public Response<ErrorResponse> handleAPIErrorException(APIError e) {
+        return new Response<>(new ErrorResponse(e.getCode(), e.getMessage()));
     }
 
 }
