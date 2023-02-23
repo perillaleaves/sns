ab8d71585f67dadf417ae8c302c512397f825dd3
gyunny <needle12s@gmail.com>
2023-02-23 14:14:50 +0900

    feat : WebConfig allowedOrigins Url

--- src/main/java/project/config/WebConfig.java
+++ src/main/java/project/config/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://mullaepro.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Access-Control-Allow-Origin",
                         "*",
